c539991a8f9928bd77eff1acd1e57e6dd5e68f29
Vyom Singhal <vyomsinghal81@gmail.com>
2026-06-04 11:06:01 +0530

    program to display battery level status

--- classwork/iteration/battery charging.py
+++
@@ -1,8 +0,0 @@
-#program to display the battery charging status
-battery_level = 20 
-
-while battery_level <= 100:
-	print("Battery level: ",battery_level,"%")
-	battery_level += 10
-	
-print("Battery fully charged!")
