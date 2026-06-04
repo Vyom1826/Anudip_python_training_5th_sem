e7e4e8d194bb94a9f80b700bb21b919cdaf4f973
Vyom Singhal <vyomsinghal81@gmail.com>
2026-06-04 11:35:59 +0530

    Program to display water tank level

---
+++ classwork/iteration/water tank.py
@@ -0,0 +1,7 @@
+#program to display the water tank level
+water_level = 0
+while water_level <= 100:
+    print("Water level:", water_level, "%")
+    water_level += 10
+
+print("Water tank fully filled!")
