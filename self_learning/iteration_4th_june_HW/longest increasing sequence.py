e4d667b6e8f4d7f20272bce7a7f2f1f7426d825e
Vyom Singhal <vyomsinghal81@gmail.com>
2026-06-05 13:21:49 +0530

    program count the length of the longest increasing sequence in a given list

---
+++ self_learning/iteration_4th_june_HW/longest increasing sequence.py
@@ -0,0 +1,20 @@
+# program count the length of the longest increasing subsequence in a given list of numbers
+
+n = int(input("Enter the number of elements in the list: ")) # input from user, number of elements in the list
+print("Enter the elements of the list: ") # prompt user to enter the elements of the list
+
+prev = int(input()) # input from user, first element of the list
+count = 1 # initialize count of longest increasing subsequence to 1
+max_count = 1 # initialize max_count to 1
+
+for _ in range(1, n):
+    curr = int(input()) # input from user, current element of the list
+    if curr > prev:
+        count += 1
+    else:
+        count = 1
+    if count > max_count: # if count exceeds the maximum count found so far, update max_count
+        max_count = count
+    prev = curr
+
+print("The length of the longest increasing subsequence is:", max_count)
