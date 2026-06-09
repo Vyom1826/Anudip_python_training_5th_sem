10ee1adee86299577996c5f617282cc4afe15608
Vyom Singhal <vyomsinghal81@gmail.com>
2026-06-09 15:12:36 +0530

    module of 2D figures area and perimeter

---
+++ classwork/functions_in_python/2Dfigures.py
@@ -0,0 +1,34 @@
+# functions of rectangle 
+# function to calculate perimeter of rectangle  
+def perimeter_rectangle(length,breath):
+    perimeter = 2 * (length + breath)
+    return perimeter
+
+# function to calculate area of rectangle
+def area_rectangle(length,breath):
+    area = length * breath
+    return area
+
+
+# functions of square
+# function to calculate perimeter of square 
+def perimeter_square(side):
+    perimeter = 4 * side
+    return perimeter
+
+# function to calculate area of square
+def area_square(side):
+    area = side * side
+    return area
+
+
+# function of circle 
+# function to calculate perimeter of circle
+def perimeter_circle(radius):
+    perimeter = 2 * 3.14 * radius
+    return perimeter
+
+# function to calculate area of circle
+def area_circle(radius):
+    area = 3.14 * radius * radius
+    return area
